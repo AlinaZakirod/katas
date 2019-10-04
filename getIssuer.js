ff74ea1e901abfc9a1d8dbf97c7ff2f8cadfe242
AlinaZakirod <gabie1989@gmail.com>
2019-10-04 12:50:38 -0400

    get Issuer of Credit Card

--- getIssuer.js
+++ getIssuer.js
@@ -25,5 +25,11 @@ function getIssuer(number) {
 //    getIssuer(340999887688123)
 
 /*********************************** the best solution ***********************************/
-
-const century = year => Math.ceil(year / 100);
+function getIssuer(n) {
+  var s = n.toString();
+  if (/^3[4|7]\d{13}$/.test(s)) return "AMEX";
+  if (/^6011\d{12}$/.test(s)) return "Discover";
+  if (/^5[1-5]\d{14}$/.test(s)) return "Mastercard";
+  if (/^4(\d{12}|\d{15})$/.test(s)) return "VISA";
+  return "Unknown";
+}
--- isItUpperCase.js
+++ isItUpperCase.js
@@ -1,1 +1,20 @@
-// testing Git hub bugs
+/****************************************************************************************
+ * https: https://www.codewars.com/kata/is-the-string-uppercase/train/javascript
+ * Task: Is the string uppercase?
+Task
+Add the isUpperCase method to String to see whether the string is ALL CAPS. For example:
+
+
+/*****************************************************************************************/
+
+
+/*************************************** my solution *************************************/
+String.prototype.isUpperCase = function() {
+  if(this == this.toUpperCase()){
+    return true
+} else return false
+}
+
+/*********************************** the best solution ***********************************/
+String.prototype.isUpperCase=function() {return this==this.toUpperCase()}
+
